e47593ee5a1e6819b9fe9fb710a02ac5b8b20756
Leo Battaglia <ms@raptorizm.com>
2021-06-09 19:49:00 +0200

    Version 0.1.3

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Parser = void 0;
+exports.Coder = void 0;
+var html = require("html");
 //Classes
-class Parser {
-}
-exports.Parser = Parser;
+var Coder = /** @class */ (function () {
+    //Constructor
+    function Coder(mode) {
+        this.mode_css = 1;
+        this.mode_html = 2;
+        this.mode_json = 3;
+        this.mode_svg = 4;
+        this.mode_xml = 5;
+        this.mode = mode;
+    }
+    //Methods
+    Coder.prototype.createEmptyPage = function () {
+        return this.getTag("doctype");
+    };
+    Coder.prototype.getTag = function (name) {
+        for (var _i = 0, _a = html.tags; _i < _a.length; _i++) {
+            var tag = _a[_i];
+            if (tag.name === name) {
+                return tag.tag;
+            }
+        }
+        return undefined;
+    };
+    return Coder;
+}());
+exports.Coder = Coder;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,30 @@
-//Classes
-export class Parser{
+const html = require("html");
 
+//Classes
+export class Coder{
+    //Declarations
+    mode:number;
+    mode_css:number = 1;
+    mode_html:number = 2;
+    mode_json:number = 3;
+    mode_svg:number = 4;
+    mode_xml:number = 5;
+
+    //Constructor
+    constructor(mode:number){
+        this.mode = mode;
+    }
+
+    //Methods
+    createEmptyPage():string{
+        return this.getTag("doctype");
+    }
+    getTag(name:string):string{
+        for(let tag of html.tags){
+            if(tag.name === name){
+                return tag.tag;
+            }
+        }
+        return undefined;
+    }
 }
