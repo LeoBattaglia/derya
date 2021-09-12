16d8d19e47a10631eef3d93352c1d6adedd81692
Leo Battaglia <ms@raptorizm.com>
2021-09-12 10:40:27 +0200

    Version 0.1.42

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 //Exports
-export {Generator} from "./lib/html_create_ts_functions";
 export {HTMLElement} from "./lib/html_element";
 export {HTMLSourceCode} from "./lib/html_sourcecode";
 export {HTMLTagAttribute} from "./lib/html_tag_attribute";
